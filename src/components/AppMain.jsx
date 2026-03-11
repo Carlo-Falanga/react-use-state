037e4b606127bd1ea20099764b8b09d44fb8f457
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-11 12:56:48 +0100

    add basic static structure and css reset rule

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -4,13 +4,18 @@ export default function AppMain() {
 
     return(
         <>
-        <div>
-            <button>HTML</button>
-            <button>CSS</button>
-            <button>JavaScript</button>
-            <button>Node.js</button>
-            <button>Express</button>
-            <button>reactJS</button>
+        <div className="buttons">
+            <button className="btn">HTML</button>
+            <button className="btn">CSS</button>
+            <button className="btn">JavaScript</button>
+            <button className="btn">Node.js</button>
+            <button className="btn">Express</button>
+            <button className="btn">reactJS</button>
+        </div>
+
+        <div className="card">
+            <h5>Card Title</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, recusandae.</p>
         </div>
         </>
     )
