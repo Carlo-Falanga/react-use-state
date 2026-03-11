9b265f85117655702dcb9ecf3b62b6a90da407de
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-11 13:21:26 +0100

    add array of objects and general fixes

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,22 +1,56 @@
+const languages = [
+  {
+    id: 1,
+    title: "HTML",
+    description: "HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini."
+  },
+  {
+    id: 2,
+    title: "CSS",
+    description: "CSS (Cascading Style Sheets) è un linguaggio di stile utilizzato per descrivere la presentazione di un documento scritto in HTML o XML. Controlla il layout, i colori, i caratteri e l'aspetto complessivo di una pagina web."
+  },
+  {
+    id: 3,
+    title: "JavaScript",
+    description: "JavaScript è un linguaggio di programmazione dinamico utilizzato per lo sviluppo web, che consente contenuti interattivi come grafici animati, pulsanti cliccabili e altre funzionalità dinamiche sui siti web."
+  },
+  {
+    id: 4,
+    title: "Node.js",
+    description: "Node.js è un ambiente di runtime JavaScript lato server che permette di utilizzare JavaScript anche sul backend, consentendo agli sviluppatori di usare lo stesso linguaggio sia sul client che sul server."
+  },
+  {
+    id: 5,
+    title: "Express",
+    description: "Express è un framework per applicazioni web minimalista e veloce per Node.js. Fornisce strumenti robusti per creare applicazioni web e API e semplifica la gestione delle rotte e delle richieste."
+  },
+  {
+    id: 6,
+    title: "ReactJS",
+    description: "ReactJS è una libreria JavaScript per costruire interfacce utente, in particolare applicazioni a pagina singola. Consente agli sviluppatori di creare componenti UI riutilizzabili e di gestire efficacemente lo stato dell'applicazione."
+  }
+];
+
+
 export default function AppMain() {
+  return (
+    <main>
+      <div className="buttons">
+        <button className="btn">HTML</button>
+        <button className="btn">CSS</button>
+        <button className="btn">JavaScript</button>
+        <button className="btn">Node.js</button>
+        <button className="btn">Express</button>
+        <button className="btn">reactJS</button>
+      </div>
 
-
-
-    return(
-        <>
-        <div className="buttons">
-            <button className="btn">HTML</button>
-            <button className="btn">CSS</button>
-            <button className="btn">JavaScript</button>
-            <button className="btn">Node.js</button>
-            <button className="btn">Express</button>
-            <button className="btn">reactJS</button>
-        </div>
-
-        <div className="card">
-            <h5>Card Title</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, recusandae.</p>
-        </div>
-        </>
-    )
+      <div className="card">
+        <h5 className="card_title">Card Title</h5>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+          recusandae.
+        </p>
+      </div>
+    </main>
+  );
 }
