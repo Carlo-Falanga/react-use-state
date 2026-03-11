79ee695bb4fda58644dc363c42e0aca1e0271e43
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-11 13:30:38 +0100

    render cards dynamically using map

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,35 +2,40 @@ const languages = [
   {
     id: 1,
     title: "HTML",
-    description: "HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini."
+    description:
+      "HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.",
   },
   {
     id: 2,
     title: "CSS",
-    description: "CSS (Cascading Style Sheets) è un linguaggio di stile utilizzato per descrivere la presentazione di un documento scritto in HTML o XML. Controlla il layout, i colori, i caratteri e l'aspetto complessivo di una pagina web."
+    description:
+      "CSS (Cascading Style Sheets) è un linguaggio di stile utilizzato per descrivere la presentazione di un documento scritto in HTML o XML. Controlla il layout, i colori, i caratteri e l'aspetto complessivo di una pagina web.",
   },
   {
     id: 3,
     title: "JavaScript",
-    description: "JavaScript è un linguaggio di programmazione dinamico utilizzato per lo sviluppo web, che consente contenuti interattivi come grafici animati, pulsanti cliccabili e altre funzionalità dinamiche sui siti web."
+    description:
+      "JavaScript è un linguaggio di programmazione dinamico utilizzato per lo sviluppo web, che consente contenuti interattivi come grafici animati, pulsanti cliccabili e altre funzionalità dinamiche sui siti web.",
   },
   {
     id: 4,
     title: "Node.js",
-    description: "Node.js è un ambiente di runtime JavaScript lato server che permette di utilizzare JavaScript anche sul backend, consentendo agli sviluppatori di usare lo stesso linguaggio sia sul client che sul server."
+    description:
+      "Node.js è un ambiente di runtime JavaScript lato server che permette di utilizzare JavaScript anche sul backend, consentendo agli sviluppatori di usare lo stesso linguaggio sia sul client che sul server.",
   },
   {
     id: 5,
     title: "Express",
-    description: "Express è un framework per applicazioni web minimalista e veloce per Node.js. Fornisce strumenti robusti per creare applicazioni web e API e semplifica la gestione delle rotte e delle richieste."
+    description:
+      "Express è un framework per applicazioni web minimalista e veloce per Node.js. Fornisce strumenti robusti per creare applicazioni web e API e semplifica la gestione delle rotte e delle richieste.",
   },
   {
     id: 6,
     title: "ReactJS",
-    description: "ReactJS è una libreria JavaScript per costruire interfacce utente, in particolare applicazioni a pagina singola. Consente agli sviluppatori di creare componenti UI riutilizzabili e di gestire efficacemente lo stato dell'applicazione."
-  }
+    description:
+      "ReactJS è una libreria JavaScript per costruire interfacce utente, in particolare applicazioni a pagina singola. Consente agli sviluppatori di creare componenti UI riutilizzabili e di gestire efficacemente lo stato dell'applicazione.",
+  },
 ];
-
 
 export default function AppMain() {
   return (
@@ -44,13 +49,14 @@ export default function AppMain() {
         <button className="btn">reactJS</button>
       </div>
 
-      <div className="card">
-        <h5 className="card_title">Card Title</h5>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
-          recusandae.
-        </p>
-      </div>
+      {languages.map((item) => (
+        <div className="card" key={item.id}>
+          <h5 className="card_title">{item.title}</h5>
+          <p>
+            {item.description}
+          </p>
+        </div>
+      ))}
     </main>
   );
 }
